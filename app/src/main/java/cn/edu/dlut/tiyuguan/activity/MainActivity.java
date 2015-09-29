26110f455e5086853881340994b923627648c4e5
dutxueyu <18840822700@163.com>
2015-09-29 21:55:09 +0800

    update test from leo

--- app/src/main/java/cn/edu/dlut/tiyuguan/activity/MainActivity.java
+++ app/src/main/java/cn/edu/dlut/tiyuguan/activity/MainActivity.java
@@ -43,7 +43,7 @@ public class MainActivity extends FragmentActivity
 	private String passWord = "";
 	private Boolean rememberme = false;
 	//测试Git临时加的注释，用后即删。。。。。薛0929
-
+//提交测试啊啊啊啊啊啊啊啊啊啊啊
 	private int resImage[] = {R.drawable.tab_home_normal,R.drawable.tab_book_normall,R.drawable.tab_aboutme_normal};
     private int resImagePressed[] = {R.drawable.tab_home_pressed,R.drawable.tab_book_pressed,R.drawable.tab_aboutme_pressed};
     private String[] textView = {"首页","预约","我的"};
