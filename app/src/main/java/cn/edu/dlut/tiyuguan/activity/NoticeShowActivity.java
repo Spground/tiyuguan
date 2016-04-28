d3c77af16926cbbb552d4fd5e42b3e8759766082
wujie <958340585@qq.com>
2016-04-28 15:42:42 +0800

    delete rubbish

--- app/src/main/java/cn/edu/dlut/tiyuguan/activity/NoticeShowActivity.java
+++ app/src/main/java/cn/edu/dlut/tiyuguan/activity/NoticeShowActivity.java
@@ -2,6 +2,8 @@ package cn.edu.dlut.tiyuguan.activity;
 
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import cn.edu.dlut.tiyuguan.R;
 import cn.edu.dlut.tiyuguan.base.BaseUi;
@@ -17,12 +19,15 @@ public class NoticeShowActivity extends BaseUi implements BaseUi.AsyncLoadDataFr
     DaoSession daoSession;
     NoticeBeanDao noticeBeanDao;
     long noticeId;
+    private TextView noticeTitleTV, noticeTimeTV, noticePublisherTV;
+    private EditText noticeContentET;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notice_show);
         noticeId = getIntent().getLongExtra(EXTRA_NOTICE_ID, -1);
         initActionBar("体育馆通知");
+        initView();
         showProgressDlg();
         asynLoadDataFromDB(this, NameConstant.dbName);
     }
@@ -38,11 +43,21 @@ public class NoticeShowActivity extends BaseUi implements BaseUi.AsyncLoadDataFr
         }
     }
 
+    private void initView() {
+        noticeTitleTV = (TextView) findViewById(R.id.notice_title);
+        noticeTimeTV =  (TextView) findViewById(R.id.notice_time);
+        noticeContentET =  (EditText) findViewById(R.id.notice_content);
+        noticePublisherTV = (TextView) findViewById(R.id.notice_publisher);
+    }
+
     private void showNotice(NoticeBean bean) {
         String noticeTitle = bean.getNoticeTitle();
         String noticeContent = bean.getNoticeContent();
         String noticeTime = bean.getNoticeTime();
         String noticePublisher = bean.getNoticePublisher();
-        ToastUtil.showErrorToast(this, noticeContent + noticePublisher + noticeTime + noticeTitle);
+        noticeContentET.setText(noticeContent);
+        noticePublisherTV.setText(noticePublisher);
+        noticeTimeTV.setText(noticeTime);
+        noticeTitleTV.setText(noticeTitle);
     }
 }
